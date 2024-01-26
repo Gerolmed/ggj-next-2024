98aa5ca05854299f00893e1ab3af1a00dadb8e6c
johannesjumpertz <johannes.jumpertz@isys.de>
2024-01-26 23:52:43 +0100

    Fixed syntax errors

--- include/node.h
+++ include/node.h
@@ -5,12 +5,22 @@
 #include "include/math.h"
 
 class Node {
+
+public:
+    virtual ~Node() = default;
+    Node();
+
     Node* parent;
-	V2 position;
-    float rotation,
+	V2 position = V2();
+    float rotation = 0;
+
+
 
     Matrix3f getRelativeMatrix();
     Matrix3f getAbsoluteMatrix();
-}
+
+    virtual void update();
+    virtual void render();
+};
 
 #endif //NODE_H
