64315c2919d19bd4373616dd6386f53bc98936aa
Luis Dietl <interruptedThread@gmail.com>
2024-01-27 11:59:10 +0100

    Added collider building

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,12 @@
 #include "include/game.h"
 
-#define TILE_SIZE 30
+#include <assert.h>
 
 void game_Init(Level* level, u32 stage, Arena* arena)
 {
+    level->static_collider = 0;
+    level->collider_count = 0;
+
     level->grid_width = 10;
     level->grid_height = 10;
     level->grid = (u8*) push_size(arena, 
@@ -15,6 +18,15 @@ void game_Init(Level* level, u32 stage, Arena* arena)
                 type = 1;
             }
             level->grid[x + y * level->grid_width] = type;
+
+            if (type) {
+                Collider collider;
+                collider.aabb.position.x = x * TILE_SIZE;
+                collider.aabb.position.y = y * TILE_SIZE;
+                collider.aabb.size.x = TILE_SIZE;
+                collider.aabb.size.y = TILE_SIZE;
+                game_PushCollider(level, collider, true);
+            }
         }
     }
     level->grid[4 + level->grid_width * 3] = 1;
@@ -37,4 +49,20 @@ void game_RenderGrid(CommandBuffer* cmd, Level* level, TextureHandle* texture)
             }
         }
     }
+}
+
+void game_PushCollider(Level* level, Collider c, bool static_c)
+{
+    assert(level->collider_count < MAX_COLLIDERS);
+    level->collider[level->collider_count] = c;
+    level->collider_count += 1;
+
+    if (static_c) {
+        level->static_collider += 1;
+    }
+}
+
+void game_ClearDynamicCollider(Level* level)
+{
+    level->collider_count = level->static_collider;
 }
